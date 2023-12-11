d93068dc50c4941023bfe350d8fe666909248082
ManasArs13 <manasyan.a.s@ya.ru>
2023-12-11 22:58:10 +0300

    Fix admin lte

--- resources/views/residual/index.blade.php
+++ resources/views/residual/index.blade.php
@@ -13,130 +13,126 @@
 @stop
 
 @section('content')
-    <div class="">
-        <div class="card">
-            <div class="card-header d-flex align-items-center justify-content-between">
+<div class="card">
+    <div class="card-header d-flex align-items-center justify-content-between">
 
-                <div class="d-flex flex-grow-1">
-                    <div class="card-tools mx-2">
-                        @if(url()->current() == route('residual.blocksMaterials'))
-                        <a href="{{ route('residual.blocksMaterials') }}" class="btn btn-info">{{__("column.blocks_materials")}}</a>
-                        @else
-                        <a href="{{ route('residual.blocksMaterials') }}" class="btn btn-primary">{{__("column.blocks_materials")}}</a>
-                        @endif
-                    </div>
-                    <div class="card-tools mx-2">
-                        @if(url()->current() == route('residual.blocksCategories'))
-                        <a href="{{ route('residual.blocksCategories') }}" class="btn btn-info">{{__("column.blocks_categories")}}</a>
-                        @else
-                        <a href="{{ route('residual.blocksCategories') }}" class="btn btn-primary">{{__("column.blocks_categories")}}</a>
-                        @endif
-                    </div>
-                    <div class="card-tools mx-2">
-                        @if(url()->current() == route('residual.blocksProducts'))
-                        <a href="{{ route('residual.blocksProducts') }}" class="btn btn-info">{{__("column.blocks_products")}}</a>
-                        @else
-                        <a href="{{ route('residual.blocksProducts') }}" class="btn btn-primary">{{__("column.blocks_products")}}</a>
-                        @endif
-                    </div>
-                    <div class="card-tools mx-2">
-                        @if(url()->current() == route('residual.concretesMaterials'))
-                        <a href="{{ route('residual.concretesMaterials') }}" class="btn btn-info">{{__("column.concretes_materials")}}</a>
-                        @else
-                        <a href="{{ route('residual.concretesMaterials') }}" class="btn btn-primary">{{__("column.concretes_materials")}}</a>
-                        @endif
-                    </div>
-                </div>
-                <div class="d-flex">
-                    <div class="card-tools mx-2">
-                        @if(url()->current() == route('residual'))
-                        <a href="{{ route('residual') }}" class="btn btn-info">{{__("column.all")}}</a>
-                        @else
-                        <a href="{{ route('residual') }}" class="btn btn-primary">{{__("column.all")}}</a>
-                        @endif
-                    </div>
-                </div>
-
+        <div class="d-flex flex-grow-1">
+            <div class="card-tools mx-1">
+                @if(url()->current() == route('residual.blocksMaterials'))
+                <a href="{{ route('residual.blocksMaterials') }}" class="btn btn-info">{{__("column.blocks_materials")}}</a>
+                @else
+                <a href="{{ route('residual.blocksMaterials') }}" class="btn btn-primary">{{__("column.blocks_materials")}}</a>
+                @endif
             </div>
-
-
-
+            <div class="card-tools mx-1">
+                @if(url()->current() == route('residual.blocksCategories'))
+                <a href="{{ route('residual.blocksCategories') }}" class="btn btn-info">{{__("column.blocks_categories")}}</a>
+                @else
+                <a href="{{ route('residual.blocksCategories') }}" class="btn btn-primary">{{__("column.blocks_categories")}}</a>
+                @endif
+            </div>
+            <div class="card-tools mx-1">
+                @if(url()->current() == route('residual.blocksProducts'))
+                <a href="{{ route('residual.blocksProducts') }}" class="btn btn-info">{{__("column.blocks_products")}}</a>
+                @else
+                <a href="{{ route('residual.blocksProducts') }}" class="btn btn-primary">{{__("column.blocks_products")}}</a>
+                @endif
+            </div>
+            <div class="card-tools mx-1">
+                @if(url()->current() == route('residual.concretesMaterials'))
+                <a href="{{ route('residual.concretesMaterials') }}" class="btn btn-info">{{__("column.concretes_materials")}}</a>
+                @else
+                <a href="{{ route('residual.concretesMaterials') }}" class="btn btn-primary">{{__("column.concretes_materials")}}</a>
+                @endif
+            </div>
         </div>
-        <div class="card-body p-0 mb-5" style="overflow-y: scroll;">
-            <table class="table table-head-fixed text-nowrap">
-                <thead>
-                    <tr>
-                        <th>
-                            {{__("column.name")}}
-                        </th>
-                        <th>
-                            {{__("column.status_ms_id")}}
-                        </th>
-                        <th>
-                            {{__("column.residual_norm")}}
-                        </th>
-                        <th>
-                            {{__("column.residual")}}
-                        </th>
-                        <th>
-                            {{__("column.need")}}
-                        </th>
-                        <th>
-                            {{__("column.making_dais")}}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($products as $product)
-                    @if($product->residual_norm)
-                    <tr>
-                        <th>
-                            {{ $product->name}}
-                        </th>
-                        <th>
-                            @if($product->residual_norm !== 0
-                            && $product->residual_norm !== null)
-
-                            <div @if (round(($product->residual /$product->residual_norm ) * 100) <= 30) class="td-percent-red" @elseif(round(($product->residual /$product->residual_norm ) * 100) > 30 && round(($product->residual /$product->residual_norm ) * 100) <= 70) class="td-percent-yellow" @else class="td-percent" @endif>
-                                        {{round(($product->residual /$product->residual_norm ) * 100)}}%
-                            </div>
-
-                            @else
-                            {{ __("column.no") }}
-                            @endif
-                        </th>
-                        <th>
-                            @if($product->residual_norm)
-                            {{ $product->residual_norm }}
-                            @else
-                            {{ __("column.no") }}
-                            @endif
-                        </th>
-                        <th>
-                            @if($product->min_balance_mc)
-                            {{ $product->min_balance_mc }}
-                            @else
-                            {{ __("column.no") }}
-                            @endif
-                        </th>
-                        <th>
-                            {{ __("column.no") }}
-                        </th>
-                        <th>
-                            {{ __("column.no") }}
-
-                        </th>
-
-                    </tr>
-                    @endif
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="cont">
-
+        <div class="d-flex">
+            <div class="card-tools mx-1">
+                @if(url()->current() == route('residual'))
+                <a href="{{ route('residual') }}" class="btn btn-info">{{__("column.all")}}</a>
+                @else
+                <a href="{{ route('residual') }}" class="btn btn-primary">{{__("column.all")}}</a>
+                @endif
             </div>
+        </div>
+
+    </div>
+
+    <div class="card-body px-0 py-2 wrapper" style="overflow-y: scroll;">
+        <table class="table table-head-fixed text-nowrap">
+            <thead>
+                <tr>
+                    <th>
+                        {{__("column.name")}}
+                    </th>
+                    <th>
+                        {{__("column.status_ms_id")}}
+                    </th>
+                    <th>
+                        {{__("column.residual_norm")}}
+                    </th>
+                    <th>
+                        {{__("column.residual")}}
+                    </th>
+                    <th>
+                        {{__("column.need")}}
+                    </th>
+                    <th>
+                        {{__("column.making_dais")}}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+                @if($product->residual_norm)
+                <tr>
+                    <th>
+                        {{ $product->name}}
+                    </th>
+                    <th>
+                        @if($product->residual_norm !== 0
+                        && $product->residual_norm !== null)
+
+                        <div @if (round(($product->residual /$product->residual_norm ) * 100) <= 30) class="td-percent-red" @elseif(round(($product->residual /$product->residual_norm ) * 100) > 30 && round(($product->residual /$product->residual_norm ) * 100) <= 70) class="td-percent-yellow" @else class="td-percent" @endif>
+                                    {{round(($product->residual /$product->residual_norm ) * 100)}}%
+                        </div>
+
+                        @else
+                        {{ __("column.no") }}
+                        @endif
+                    </th>
+                    <th>
+                        @if($product->residual_norm)
+                        {{ $product->residual_norm }}
+                        @else
+                        {{ __("column.no") }}
+                        @endif
+                    </th>
+                    <th>
+                        @if($product->min_balance_mc)
+                        {{ $product->min_balance_mc }}
+                        @else
+                        {{ __("column.no") }}
+                        @endif
+                    </th>
+                    <th>
+                        {{ __("column.no") }}
+                    </th>
+                    <th>
+                        {{ __("column.no") }}
+
+                    </th>
+
+                </tr>
+                @endif
+                @endforeach
+            </tbody>
+        </table>
+        <div class="cont">
+
         </div>
     </div>
+</div>
 @stop
 
 @section('js')
