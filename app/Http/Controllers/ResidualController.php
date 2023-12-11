f34ee8d313d62fb585638eff6a1bae448ffbe649
ManasArs13 <manasyan.a.s@ya.ru>
2023-12-11 21:10:59 +0300

    Update ResidualController.php

--- app/Http/Controllers/ResidualController.php
+++ app/Http/Controllers/ResidualController.php
@@ -11,7 +11,8 @@ class ResidualController extends Controller
     {
         $needMenuForItem = true;
 
-        $products = Product::where('type', Product::PRODUCTS)
+        $products = Product::where('residual_norm', '<>', null)
+            ->where('type', Product::PRODUCTS)
             ->orWhere('type', Product::MATERIAL)
             ->get();
         $entity = 'residuals';
@@ -25,6 +26,7 @@ class ResidualController extends Controller
         $needMenuForItem = true;
 
         $products = Product::query()
+            ->where('residual_norm', '<>', null)
             ->where('type', Product::MATERIAL)
             ->where('building_material', Product::BLOCK)->get()->sortBy('sort');
         $entity = 'residuals';
@@ -36,7 +38,18 @@ class ResidualController extends Controller
     {
         $needMenuForItem = true;
 
-        $products = ProductsCategory::query()->where('building_material', ProductsCategory::BLOCK)->get();
+        $products = ProductsCategory::query()
+            ->where('building_material', ProductsCategory::BLOCK)->get();
+
+        foreach ($products as $product) {
+            $residual =  Product::query()->where('type', Product::PRODUCTS)->where('category_id', $product->id)->get()->sum('residual');
+            $residual_norm = Product::query()->where('type', Product::PRODUCTS)->where('category_id', $product->id)->get()->sum('residual_norm');
+            if ($residual_norm !== 0) {
+                $product->residual = $residual;
+                $product->residual_norm = $residual_norm;
+            }
+        }
+
         $entity = 'residuals';
 
         return view("residual.index", compact("needMenuForItem", "entity", "products"));
@@ -47,6 +60,7 @@ class ResidualController extends Controller
         $needMenuForItem = true;
 
         $products = Product::query()
+            ->where('residual_norm', '<>', null)
             ->where('type', Product::PRODUCTS)
             ->where('building_material', Product::BLOCK)->get()->sortBy('sort');
         $entity = 'residuals';
@@ -60,7 +74,9 @@ class ResidualController extends Controller
     {
         $needMenuForItem = true;
 
-        $products = Product::query()->where('building_material',Product::CONCRETE)->get()->sortBy('sort');
+        $products = Product::query()
+            ->where('residual_norm', '<>', null)
+            ->where('building_material', Product::CONCRETE)->get()->sortBy('sort');
         $entity = 'residuals';
 
         return view("residual.index", compact("needMenuForItem", "entity", "products"));
