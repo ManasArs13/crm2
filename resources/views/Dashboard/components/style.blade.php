32f9b56a394808b0b554556567e810192b363ea9
ManasArs13 <manasyan.a.s@ya.ru>
2023-12-12 11:05:11 +0300

    fix style

--- resources/views/Dashboard/components/style.blade.php
+++ resources/views/Dashboard/components/style.blade.php
@@ -1,17 +1,16 @@
 <style>
-    #loadingIndicator{
+    .content {
+        background-color: #f4f6f9;
+    }
+
+    #loadingIndicator {
         position: sticky;
-        top:650px;
+        top: 650px;
         left: 800px;
         z-index: 1000;
     }
-    #card2{
-        /* min-width: 1220px; */
-    }
-    #card3{
-        min-width: 340px;
-    }
-    #status{
+
+    #status {
         width: auto;
         height: 40px;
         border-radius: 5px;
@@ -19,24 +18,8 @@
         justify-content: center;
         align-items: center;
     }
-    /* #datepicker{
-        padding: 10px 10px 10px 40px;
-        border: 1px solid #ccc;
-        border-radius: 3px;
-        background-color: darkgrey;
-        margin-top: 8px;
-        width: 200px;
-        height: 45px;
-        font-size: 21px;
-        outline: #ccc !important;
-    } */
-    #datepicker:hover{
-        background-color: #c4c2c2;
-    }
-    #datepicker:focus-visible {
-        outline: none !important;
-    }
-    .button-block{
+
+    .button-block {
         height: 60px;
         border-radius: 3px;
         margin-left: 10px;
@@ -44,24 +27,28 @@
         justify-content: start;
         align-items: center;
     }
-    .buttons{
-        width:900px ;
+
+    .buttons {
+        width: 900px;
         display: flex;
         justify-content: space-evenly;
         align-items: center;
     }
-    #buttons{
+
+    #buttons {
         width: 120px;
         height: 45px;
         background-color: darkgrey;
         font-weight: bold;
         font-family: sans-serif;
     }
-    #buttons:hover{
+
+    #buttons:hover {
         background-color: #c4c2c2;
 
     }
-    .td-percent{
+
+    .td-percent {
         min-width: 40px;
         max-width: 60px;
         height: 25px;
@@ -72,7 +59,8 @@
         align-items: center;
         border-radius: 4px;
     }
-    .td-percent-red{
+
+    .td-percent-red {
         min-width: 40px;
         max-width: 60px;
         height: 25px;
@@ -84,7 +72,7 @@
         border-radius: 4px;
     }
 
-    .td-percent-yellow{
+    .td-percent-yellow {
         min-width: 40px;
         max-width: 60px;
         height: 25px;
--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -42,12 +42,9 @@
                         </label>
                     </div>
                 </div>
-
             </div>
-
             <div class="card-body p-2 wrapper">
                 @include('Dashboard.components.canvas')
-
             </div>
         </div>
         <div class="card">
@@ -120,99 +117,5 @@
         </div>
     </div>
 </div>
-
-
-<!-- <div class="py-3 d-flex justify-content-between">
-    <div id="card2" class="card bg-gray-light mr-3">
-        <div class="button-block">
-            <div class="buttons">
-                <div class="card-tools w-75 justify-content-end d-flex">
-                    <label style="display: block;">
-                        <input type="date" id="datepicker" value="{{ \Illuminate\Support\Carbon::now()->format('Y-m-d') }}">
-                    </label>
-                </div>
-                <div class="card-tools w-100 h-75 justify-content-end d-flex">
-                    <a href="{{route('admin.dashboard',['filter'=>'now'])}}" id="buttons" class="btn " @if(request()->filter == 'now') style="border: solid 2px #676666" @endif>СЕГОДНЯ</a>
-                </div>
-                <div class="card-tools w-100 h-75 justify-content-end d-flex">
-                    <a href="{{route('admin.dashboard',['filter'=>'tomorrow'])}}" id="buttons" class="btn " @if(request()->filter == 'tomorrow') style="border: solid 2px #676666" @endif>ЗАВТРА</a>
-                </div>
-                <div class="card-tools w-100 h-75 justify-content-end d-flex">
-                    <a href="{{route('admin.dashboard',['filter'=>'three-day'])}}" id="buttons" class="btn " @if(request()->filter == 'three-day') style="border: solid 2px #676666" @endif>3 ДНЯ</a>
-                </div>
-                <div class="card-tools w-100 h-75 justify-content-end d-flex">
-                    <a href="{{route('admin.dashboard',['filter'=>'week'])}}" id="buttons" class="btn" @if(request()->filter == 'week') style="border: solid 2px #676666" @endif>НЕДЕЛЯ</a>
-                </div>
-            </div>
-        </div>
-        @include('Dashboard.components.canvas')
-        @include('Dashboard.components.load')
-        @include('Dashboard.components.orderTable',['filter'=>'index'])
-    </div>
-    <div id="card3" class="card bg-gray-light">
-        <div class="card" style="overflow-x:auto ">
-            <table cellpadding="5px">
-                <thead>
-                    <tr>
-                        <th class="d-flex justify-content-center align-items-center mb-2 ">
-                            <span style="font-size: 25px;color: #949494">Материалы</span>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($materials as $material)
-                    <tr style="border-bottom: 1px solid #dee2e6;">
-                        <td class="m-1 d-flex justify-content-between">
-                            {{$material->name}}
-                        </td>
-                        <td>
-                            @if($material->residual_norm !== 0
-                            && $material->residual_norm !== null
-                            && $material->type !== 'не выбрано')
-                            <div @if (round(($material->residual /$material->residual_norm ) * 100) <= 30) class="td-percent-red" @elseif(round(($material->residual /$material->residual_norm ) * 100) > 30 && round(($material->residual /$material->residual_norm ) * 100) <= 70) class="td-percent-yellow" @else class="td-percent" @endif>
-                                        {{round(($material->residual /$material->residual_norm ) * 100)}}%
-                            </div>
-                            @else
-                            {{null}}
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="card" style="overflow-x: auto ">
-            <table cellpadding="5px">
-                <thead>
-                    <tr>
-                        <th class="d-flex justify-content-center align-items-center mb-2">
-                            <span style="font-size: 25px;color: #949494">Товары</span>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($products as $product)
-                    <tr style="border-bottom: 1px solid #dee2e6;">
-                        <td class="m-1 d-flex justify-content-between">
-                            {{$product->name}}
-                        </td>
-                        <td>
-                            @if($product->residual_norm !== 0
-                            && $product->residual_norm !== null
-                            && $product->type !== 'не выбрано')
-                            <div class="td-percent">
-                                {{round(($product->residual /$product->residual_norm ) * 100)}}%
-                            </div>
-                            @else
-                            {{null}}
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div> -->
 @stop
 @include('Dashboard.components.style')
