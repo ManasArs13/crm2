d2c132a0c7292791228866974d62d3e7576ccf24
ManasArs13 <manasyan.a.s@ya.ru>
2023-12-26 20:01:46 +0300

    Update ProductService.php

--- app/Services/Entity/ProductService.php
+++ app/Services/Entity/ProductService.php
@@ -80,21 +80,24 @@ class ProductService implements EntityInterface
             $product = Product::where('id', '=', $residual['assortmentId'])->first();
 
             if ($product) {
-                if ($product->residual - $product->residual_norm < 0) {
-                    $product_need = abs($product->residual - $product->residual_norm);
+                if ($product->residual !== null &&  $product->residual_norm !== null) {
+                    $residual_material = 'да';
+                    if ($product->residual - $product->residual_norm < 0) {
+                        $product_need = abs($product->residual - $product->residual_norm);
 
-                    $tech_chart_product = TechChartProduct::where('product_id', '=', $residual['assortmentId'])->first();
-                    if ($tech_chart_product) {
-                        $tech_chart_materials = TechChartMaterial::where('tech_chart_id', '=', $tech_chart_product->tech_chart_id)->get();
-                        if ($tech_chart_materials) {
-                            $residual_material = 'да';
-                            foreach ($tech_chart_materials as $tech_chart_material) {
-                                $material = Product::where('id', '=', $tech_chart_material->product_id)->first();
+                        $tech_chart_product = TechChartProduct::where('product_id', '=', $residual['assortmentId'])->first();
+                        if ($tech_chart_product) {
+                            $tech_chart_materials = TechChartMaterial::where('tech_chart_id', '=', $tech_chart_product->tech_chart_id)->get();
+                            if ($tech_chart_materials) {
 
-                                $need_material = $product_need * $tech_chart_material->quantity;
-                                
-                                if ($material->residual < $need_material) {
-                                    $residual_material = 'нет';
+                                foreach ($tech_chart_materials as $tech_chart_material) {
+                                    $material = Product::where('id', '=', $tech_chart_material->product_id)->first();
+
+                                    $need_material = $product_need * $tech_chart_material->quantity;
+
+                                    if ($material->residual < $need_material) {
+                                        $residual_material = 'нет';
+                                    }
                                 }
                             }
                         }
