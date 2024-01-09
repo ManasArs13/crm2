52829f1680854d7b622a98e7bfe0f4697819cc8f
ManasArs13 <manasyan.a.s@ya.ru>
2024-01-09 18:08:37 +0300

    Update MoySkladService.php

--- app/Services/Api/MoySkladService.php
+++ app/Services/Api/MoySkladService.php
@@ -83,7 +83,7 @@ class MoySkladService
 
     public function actionGetRowsFromJson(string $url, bool $rows = true, $downloadPath = '')
     {
-        usleep(350000);
+        usleep(70000);
         $response = $this->client->request('GET', $url, [
             'headers' => [
                 'Accept-Encoding' => 'gzip',
