e56e7ada56bf217d5a73d0ca65e605a2e7881ed1
ManasArs13 <manasyan.a.s@ya.ru>
2024-01-28 20:17:03 +0300

    Update CheckContact.php

--- app/Console/Commands/CheckContact.php
+++ app/Console/Commands/CheckContact.php
@@ -33,69 +33,70 @@ class SyncContactMsAmo extends Command
 
         foreach ($contactAmos as $contactAmo) {
 
-            $contactMS = ContactMs::where('phone_norm', $contactAmo->phone_norm)->first();
+            $id = null;
+            $link = null;
 
-            if ($contactMS) {
+            if ($contactAmo->phone_norm !== null) {
 
-                $id = null;
-                $link = null;
+                $contactMS = ContactMs::where('phone_norm', $contactAmo->phone_norm)->first();
 
-                if($contactMS->phone_norm !== null) {
-                    $id = $contactMS->id;
-                    $link = 'https://api.moysklad.ru/#company/edit?id='.$contactMS->id;
+                if ($contactMS) {
+                    if ($contactMS->phone_norm !== null) {
+                        $id = $contactMS->id;
+                        $link = 'https://api.moysklad.ru/#company/edit?id=' . $contactMS->id;
+                    }
                 }
+            }
+            $accessToken = json_decode(file_get_contents(base_path('token_amocrm_widget.json')), true)['accessToken'];
 
-                $accessToken = json_decode(file_get_contents(base_path('token_amocrm_widget.json')), true)['accessToken'];
-
-                $customFieldUpdate = [
-                    "field_id" => 604475,
-                    "field_name" => "Cсылка на контрагента в моем складе",
-                    "field_code" => null,
-                    "field_type" => "url",
-                    "values" => [
-                        [
-                            "value" => $link
-                        ]
+            $customFieldUpdate = [
+                "field_id" => 604475,
+                "field_name" => "Cсылка на контрагента в моем складе",
+                "field_code" => null,
+                "field_type" => "url",
+                "values" => [
+                    [
+                        "value" => $link
                     ]
-                ];
-                $customFieldUpdate2 = [
-                    "field_id" => 604457,
-                    "field_name" => "Ид контрагента в  моем складе",
-                    "field_code" => null,
-                    "field_type" => "text",
-                    "values" => [
-                        [
-                            "value" => $id
-                        ]
+                ]
+            ];
+            $customFieldUpdate2 = [
+                "field_id" => 604457,
+                "field_name" => "Ид контрагента в  моем складе",
+                "field_code" => null,
+                "field_type" => "text",
+                "values" => [
+                    [
+                        "value" => $id
                     ]
-                ];
+                ]
+            ];
 
 
-                $client = new Client([
-                    'base_uri' => 'https://euroblock.amocrm.ru/api/v4/',
-                    'headers' => [
-                        'Authorization' => "Bearer $accessToken",
-                        'Content-Type' => 'application/json',
-                    ],
+            $client = new Client([
+                'base_uri' => 'https://euroblock.amocrm.ru/api/v4/',
+                'headers' => [
+                    'Authorization' => "Bearer $accessToken",
+                    'Content-Type' => 'application/json',
+                ],
+            ]);
+
+            try {
+                $response = $client->patch("contacts/$contactAmo->id", [
+                    'json' => ['custom_fields_values' => [$customFieldUpdate, $customFieldUpdate2]],
                 ]);
 
-                try {
-                    $response = $client->patch("contacts/$contactAmo->id", [
-                        'json' => ['custom_fields_values' => [$customFieldUpdate, $customFieldUpdate2]],
-                    ]);
+                $contactAmo->contact_ms_id = $id;
+                $contactAmo->contact_ms_link = $link;
+                $contactAmo->save();
 
-                    $contactAmo->contact_ms_id = $id;
-                    $contactAmo->contact_ms_link = $link;
-                    $contactAmo->save();
-
-                    if ($response->getStatusCode() == 200) {
-                        info('Custom field updated successfully.' . $contactAmo->id);
-                    } else {
-                        info('Error updating custom field.' . $contactAmo->id);
-                    }
-                } catch (RequestException  $e) {
-                    info($e->getMessage());
+                if ($response->getStatusCode() == 200) {
+                    info('Custom field updated successfully.' . $contactAmo->id);
+                } else {
+                    info('Error updating custom field.' . $contactAmo->id);
                 }
+            } catch (RequestException  $e) {
+                info($e->getMessage());
             }
         }
     }
